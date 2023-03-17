0eb8bf3c0111fafa07cf9b0c05c4c56ab4849228
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-17 16:46:42 +0500

    17-03-2023

---
+++ Revision Class (17-03-2023) (2208B1)/index.js
@@ -0,0 +1,131 @@
+//Variables
+
+// let myName="Sameer"
+// let age=23
+// let employed=true
+// let weight=70.76
+
+
+// alert(typeof(myName))
+// alert(typeof(age))
+// alert(typeof(employed))
+// alert(typeof(weight))
+
+//Arithmetic Operators
+// let num1=45
+// let num2=5
+
+// let add=num1+num2
+
+// alert(add)
+
+
+// let sub=num1-num2
+
+// alert(sub)
+
+
+// let rem=num1%num2
+
+// alert(rem)
+
+
+//Comparison Operators
+// alert(45==="45")
+
+// alert(65>=76)
+
+
+
+//Logical Operators
+//1. OR Operator (||)
+// document.write(5===3 || 5<3 || 5===5)
+//2. AND Operator (&&)
+// document.write(5===3 && 5>3 && 5===5)
+//3. NOT Operator (!)
+// document.write(5!==3)
+
+//User Input
+// let stdName=+prompt("Enter your number")
+
+// document.write(stdName)
+
+// alert(typeof(stdName))
+
+// let age=parseFloat(prompt("Enter your age"))
+
+// document.write(age)
+
+// alert(typeof(age))
+
+//Concatenation
+// let firstName=prompt("Enter your first name")
+// let lastName=prompt("Enter your last name")
+
+// let fullName=firstName+lastName
+
+
+// document.write(firstName+lastName)
+
+
+// document.write(fullName)
+
+// let num1=prompt("Enter first number")
+// let num2=prompt("Enter second number")
+
+// let result=num1+num2
+
+// document.write(result)
+
+
+// let firstName=prompt("Enter your first name")
+// let lastName=prompt("Enter your last name")
+
+// let fullName=firstName+" "+lastName
+
+// document.write(fullName)
+
+
+// let num1=prompt("Enter first number")
+// let num2=prompt("Enter second number")
+
+// let result=parseFloat(num1)+parseFloat(num2)
+
+// document.write(result)
+
+
+//Order of Operations
+// var exp=2+48/(2*6)-3
+
+// document.write(exp)
+
+//Example 1
+// let price=+prompt("Enter price of a product")
+// let quantity=+prompt("Enter quantity of a product")
+
+// let total=price*quantity
+
+// document.write("Your total bill is PKR "+total)
+
+//Example 2
+// let totalMarks=300
+// let subj1=+prompt("Enter subject 1 marks") //100
+// let subj2=+prompt("Enter subject 2 marks") //100
+// let subj3=+prompt("Enter subject 3 marks") //100
+
+// let obtMarks=subj1+subj2+subj3
+// let percentage=obtMarks/totalMarks*100
+
+// document.write("Your percentage is "+percentage.toFixed(2)+"%")
+
+//Take price of a product as input and discount percentage input by user
+//print final price after discount
+// let price=+parseFloat(prompt("Enter price"))
+
+// let discount=+parseFloat(prompt("Enter discount percentage"))
+
+// let finalPrice=price-(price*(discount/100))
+
+
+
+
